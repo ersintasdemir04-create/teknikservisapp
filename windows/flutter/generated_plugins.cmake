0fec222fb4647fa2e933095e7728dbc25370e612
ersin <ersintasdemir04@gmail.com>
2026-05-17 16:38:52 +0300

    fiz ios dependencies

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -7,7 +7,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_auth
   firebase_core
-  flutter_blue_plus_winrt
   permission_handler_windows
   share_plus
   url_launcher_windows
